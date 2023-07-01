bd6da5a66ae650f1192cd0b636b15deacb0b39da
avadhanisanghamitra <138146055+avadhanisanghamitra@users.noreply.github.com>
2023-07-01 11:09:25 +0530

    Add files via upload

---
+++ numpycasestudy.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+a=np.array([1,2,3,4,5])
+print(a)
+
+
+# In[3]:
+
+
+b=np.array([[1,2,3],[4,5,6]])
+
+
+# In[4]:
+
+
+print(b)
+
+
+# In[5]:
+
+
+c=np.zeros((3,4))
+
+
+# In[6]:
+
+
+print(c)
+
+
+# In[7]:
+
+
+d=np.ones((2,2))
+print(d)
+
+
+# In[8]:
+
+
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+
+
+# In[9]:
+
+
+c=a+b
+print(c)
+
+
+# In[11]:
+
+
+d=np.array([[1,2],[3,4]])
+e=np.array([[5,6],[7,8]])
+f=d*e
+print(f)
+
+
+# In[12]:
+
+
+g=np.array([[1,2],[3,4]])
+h=np.array([[5,6],[7,8]])
+i=np.dot(g,h)
+print(i)
+
+
+# In[ ]:
+
+
+
+
